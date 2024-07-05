9ab93c4a85a1b8c1571c4b231aafab288b234783
RogerTaule <roger.taulee@gmail.com>
2024-07-05 16:41:54 +0200

    Keep working

--- src/gencircom.js
+++ src/gencircom.js
@@ -13,19 +13,7 @@ module.exports.genCircom = async function genCircom(templateFile, starkInfos = [
         optionsCircom.starkInfo = starkInfos;
     }
 
-    if(basicVerificationKeys.length === 1) {
-        optionsCircom.basicVK = basicVerificationKeys[0];
-    } else {
-        optionsCircom.basicVK = basicVerificationKeys;
-    }
-
-    if(aggVerificationKeys.length === 1) {
-        optionsCircom.aggregatedVK = aggVerificationKeys[0];
-    } else {
-        optionsCircom.aggregatedVK = aggVerificationKeys;
-    }
-
-    optionsCircom.verifierFilenames = verifierFilenames;
+   
 
     if(publics.length === 1) {
         optionsCircom.publics = publics[0];
@@ -33,6 +21,9 @@ module.exports.genCircom = async function genCircom(templateFile, starkInfos = [
         optionsCircom.publics = publics;
     }
 
+    optionsCircom.basicVK = basicVerificationKeys;
+    optionsCircom.aggregatedVK = aggVerificationKeys;
+    optionsCircom.verifierFilenames = verifierFilenames;
     optionsCircom.vadcopInfo = vadcopInfo;
 
     const circomTemplate = await fs.promises.readFile(templateFile, "utf8");
