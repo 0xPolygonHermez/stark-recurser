602171ba961753af1c49f8bbd3abf7d8d5cf1e7f
RogerTaule <roger.taulee@gmail.com>
2024-07-09 14:40:52 +0200

    Fixing compressor needed

--- src/vadcop/is_compressor_needed.js
+++ src/vadcop/is_compressor_needed.js
@@ -9,17 +9,11 @@ module.exports.isCompressorNeeded = async function isCompressorNeeded(r1csFilena
     const r1cs = await readR1cs(r1csFilename, { F });
 
     const { NUsed: NUsedC18 } = getCompressorConstraints(F, r1cs, 18);
-
-    const { NUsed: NUsedC12 } = getCompressorConstraints(F, r1cs, 12);
     
     let nBitsC18 = log2(NUsedC18 - 1) + 1;
 
-    let nBitsC12 = log2(NUsedC12 - 1) + 1;
-
     if (nBitsC18 === 17) {
         return { hasCompressor: false, nCols: 18 };
-    } else if(nBitsC12 === 17) {
-        return { hasCompressor: false, nCols: 12 };
     } else {
         return { hasCompressor: true, nBits: nBitsC18 }
     }
