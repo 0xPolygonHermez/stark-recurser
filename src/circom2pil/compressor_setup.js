110be1887f3d23ac2278952272ce9ec12dd1cd07
RogerTaule <roger.taulee@gmail.com>
2025-08-28 14:25:46 +0000

    Keep working

--- src/circom2pil/compressor_setup.js
+++ src/circom2pil/compressor_setup.js
@@ -1,14 +1,17 @@
 const {readR1cs} = require("r1csfile");
-const plonkSetupCompressor = require("./compressor/compressor_setup.js");
+const compressorRecursive = require("./compressor/compressor_setup.js");
+const compressorFinal = require("./final/final_setup.js");
 const { writeExecFile } = require("./compressor_exec.js");
 
 module.exports.compressorSetup = async function compressorSetup(r1csFile, cols, options = {}) {
     
     const r1cs = await readR1cs(r1csFile, { logger:console });
 
-    if(cols != 36) throw new Error("Invalid number of cols");
+    if(cols != 36 && cols != 42) throw new Error("Invalid number of cols");
 
-    let res = await plonkSetupCompressor(r1cs, options);
+    let res = cols == 36 
+        ? await compressorRecursive(r1cs, options)
+        : await compressorFinal(r1cs, options);
 
     const exec = await writeExecFile(res.plonkAdditions, res.sMap);
 
